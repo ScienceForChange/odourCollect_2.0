1fb4c0f2434c65c52f7ef0b060df25b7b1237fdb
Carlos Millan <carlos.millan@scienceforchange.eu>
2023-11-07 16:31:59 +0100

    url en front bien

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     //appVersion: require('../../package.json').version,
-    BACKEND_BASE_URL: 'http://prod-url',
+    BACKEND_BASE_URL: 'http://odourcollect.cloud/',
     AUTHORIZATION: 'Authorization',
     BEARER: 'Bearer ',
     BACKEND_TOKEN: '',
