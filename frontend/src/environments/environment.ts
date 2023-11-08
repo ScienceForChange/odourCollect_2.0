f58230dc5d6af3147e3a6c85efb40309a266a870
Carlos Millan <carlos.millan@scienceforchange.eu>
2023-11-08 18:31:21 +0100

    parece que certificado bien pero fallan confs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     //appVersion: require('../../package.json').version,
-    BACKEND_BASE_URL: 'http://odourcollect.cloud/',
+    BACKEND_BASE_URL: 'https://odourcollect.cloud/',
     AUTHORIZATION: 'Authorization',
     BEARER: 'Bearer ',
     BACKEND_TOKEN: '',
