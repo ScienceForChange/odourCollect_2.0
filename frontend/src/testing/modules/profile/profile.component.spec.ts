a2c5348bf6ace8fd3fd0ed165213591c69e7d823
almenarasfc <alberto.almenara@scienceforchange.eu>
2024-01-30 17:04:02 +0100

    prueba2

--- frontend/src/testing/modules/profile/profile.component.spec.ts
+++ frontend/src/testing/modules/profile/profile.component.spec.ts
@@ -38,5 +38,6 @@ describe('ProfileComponent', () => {
     expect(spy).toHaveBeenCalledWith(AboutTrainedUserComponent, {  position: 'start', scroll: false, panelClass: 'about-canvas'});
 
     
+
   });
 });
