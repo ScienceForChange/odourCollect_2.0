30fb09d03575c330789cce1f26044c8a3361f12f
Carlos Millan <carlos.millan@scienceforchange.eu>
2024-02-01 18:53:13 +0100

    remove logic for certain types and subtypes cos now we good

--- api/app/Http/Controllers/OdourObservationController.php
+++ api/app/Http/Controllers/OdourObservationController.php
@@ -45,13 +45,6 @@ class OdourObservationController extends Controller
         $limit = request()->limit ?? '';
 
         $odours = $odourObservations
-                        ->whereIn('odour_sub_type_id',[
-                            10,11,12,13,14,15, // 2 Waste Water
-                            16,17,18,19,20,21,22,23,24,25, // 3 Agriculture/LiveStock
-                            26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43, // 4 Food Industries
-                            44,45,46,47,48,49,50,51,52,53,54,55,56,57, // 5 Industrial
-                            58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73 // 6 Urban
-                        ])
                         ->orderBy('created_at','DESC')->limit($limit)->get();
 
         if(request()->has('is_inside')) {
