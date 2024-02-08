9e6a6ebe670f74e05090e6826d39544cfc1cca95
almenarasfc <alberto.almenara@scienceforchange.eu>
2024-02-08 17:17:29 +0100

    Testing

--- frontend/src/testing/modules/information/about-data-protection.component.spec.ts
+++ frontend/src/testing/modules/information/about-data-protection.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutDataProtectionComponent } from './about-data-protection.component';
+import { AboutDataProtectionComponent } from '../../../app/modules/information/components/about-data-protection/about-data-protection.component';
 import { NgbActiveModal, NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { SharedComponentsModule } from 'src/app/shared/components/shared.components.module';
 
--- frontend/src/testing/modules/modals/register-modal.component.spec.ts
+++ frontend/src/testing/modules/modals/register-modal.component.spec.ts
@@ -2,12 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { RegisterModalComponent } from './register-modal.component';
+import { RegisterModalComponent } from '../../../app/modules/modals/register-modal/register-modal.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('RegisterModalComponent', () => {
   let component: RegisterModalComponent;
   let fixture: ComponentFixture<RegisterModalComponent>;
+  let routerMock: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,6 +17,7 @@ describe('RegisterModalComponent', () => {
       imports: [RouterTestingModule],
       declarations: [RegisterModalComponent]
     });
+    routerMock = TestBed.inject(Router);
     fixture = TestBed.createComponent(RegisterModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,4 +26,18 @@ describe('RegisterModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close modal', async () => {
+
+    // Espía el método de cierre del modal
+    jest.spyOn(component.modal, 'close');
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+
+    expect(component.modal.close).toHaveBeenCalled();
+
+  });
+
+  //TODO: Add more tests
+
 });
