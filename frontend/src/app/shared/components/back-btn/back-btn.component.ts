1aa993e8705f2dec86bd3f78241ad3657db8a70e
Tomás Tubert <144031996+TomasTubertSFC@users.noreply.github.com>
2024-02-12 09:15:50 +0100

    Frontend/fix/odour styles (#199)
    * Have to check header at odour form
    
    * Fixed header of add-odour form

--- frontend/src/app/shared/components/back-btn/back-btn.component.ts
+++ frontend/src/app/shared/components/back-btn/back-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -8,14 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./back-btn.component.scss'],
 })
 export class BackBtnComponent {
-  
+  @Input() backTo!: () => void;
   @Input() route: string | null = null;
 
-  constructor(private location: Location,private router:Router) {}
-  
+  constructor(
+    private location: Location,
+    private router: Router,
+  ) {}
+
   goBack() {
+    if (this.backTo) {
+      this.backTo();
+      return;
+    }
     if (this.route) this.router.navigate([this.route]);
     else this.location.back();
   }
-
 }
